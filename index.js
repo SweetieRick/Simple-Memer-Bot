151d43e567831a128c70d53db9af588d2df41bee
SuperRichyMario <65025304+SweetieRick@users.noreply.github.com>
2020-10-10 18:47:04 +0200

    Added eslint

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 const PREFIX = process.env.PREFIX;
+const fs = require("fs");
+
+// ? Command Handling
+client.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+// ? Event Handling
 
 client.on("ready", () => {
-  date = new Date();
+  const date = new Date();
   console.log(`Client logged in as ${client.user} at ${date}`);
 });
 
